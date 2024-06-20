1ba658c6674913799b34d96473e95e4918b963d5
OJl3g <oleg.tiabin@gmail.com>
2024-06-20 10:31:52 +0300

    add user panel

--- src/main/java/org/ojl3g/mvc_promo/controller/PrizeController.java
+++ src/main/java/org/ojl3g/mvc_promo/controller/PrizeController.java
@@ -4,10 +4,11 @@ import org.ojl3g.mvc_promo.model.Prize;
 import org.ojl3g.mvc_promo.service.PrizeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -15,6 +16,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class PrizeController {
@@ -42,8 +45,7 @@ public class PrizeController {
             @RequestParam String prizeName,
             @RequestParam String promoCode,
             @RequestParam() MultipartFile prizeImage
-            )
-    {
+    ) {
         String[] splitCodes = promoCode.split("[,;]");
         String linkImg;
         try {
@@ -54,7 +56,7 @@ public class PrizeController {
         }
 
         for (String code : splitCodes) {
-            Prize prize = new Prize(false, code,prizeName,linkImg);
+            Prize prize = new Prize(false, code, prizeName, linkImg);
             prizeService.addPrize(prize);
         }
 
@@ -69,10 +71,34 @@ public class PrizeController {
 
     public String saveImgToDirectory(MultipartFile multipartFile) throws IOException {
         Path path =
-                Paths.get("C:\\Users\\Олег\\IdeaProjects\\MVC_Promo\\src\\main\\resources\\static\\img\\"+multipartFile.getOriginalFilename());
-        Files.write(path,multipartFile.getBytes(), StandardOpenOption.CREATE);
-        return "/img/"+multipartFile.getOriginalFilename();
+                Paths.get("C:\\Users\\Олег\\IdeaProjects\\MVC_Promo\\src\\main\\resources\\static\\img\\" + multipartFile.getOriginalFilename());
+        Files.write(path, multipartFile.getBytes(), StandardOpenOption.CREATE);
+        return "/img/" + multipartFile.getOriginalFilename();
+    }
+
+    @GetMapping("/winners")
+    public String winnersGet(Model model) {
+        List<Prize> listPrize = prizeService.getAllPrize();
+        model.addAttribute("listPrize", listPrize);
+        return "winners";
+    }
+
+    @PostMapping("/edit_status")
+    public ResponseEntity<String> editStatus(
+            @RequestBody Map<String, Long> payload) {
+        Long prizeId = payload.get("id");
+
+        boolean updated = prizeService.changeStatus(prizeId);
+
+        if (updated) {
+            return ResponseEntity.ok("Status changed");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error changing status");
+        }
     }
 
 
 }
+
+
+
--- src/main/java/org/ojl3g/mvc_promo/service/PrizeService.java
+++ src/main/java/org/ojl3g/mvc_promo/service/PrizeService.java
@@ -4,6 +4,9 @@ import org.ojl3g.mvc_promo.model.Prize;
 import org.ojl3g.mvc_promo.repository.PromoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class PrizeService {
     PromoRepository promoRepository;
@@ -16,5 +19,21 @@ public class PrizeService {
         promoRepository.save(prize);
     }
 
+
+    public List<Prize> getAllPrize() {
+        return promoRepository.findAll();
+    }
+
+    public boolean changeStatus(Long prizeId) {
+        Optional<Prize> prizeOptional = promoRepository.findById(prizeId);
+        if (prizeOptional.isPresent()) {
+            Prize prize = prizeOptional.get();
+
+            prize.setStatus(!prize.isStatus()); // смена статуса на противоположный
+            promoRepository.save(prize);
+            return true;
+        }
+        return false;
+    }
 
 }
